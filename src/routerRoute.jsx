0fbeb625c8deaf693e0816acaedb32e2b89653a6
Tahsin <alahitahsin@gmail.com>
2025-01-14 23:19:52 +0600

    created: LoginPage.jsx

--- src/routerRoute.jsx
+++ src/routerRoute.jsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Homepage /> }],
+    children: [
+      { path: "/", element: <Homepage /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
 ]);
 
