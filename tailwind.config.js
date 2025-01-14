20328c1b7cf2c843614718c951e45bf9a73241d4
Tahsin <alahitahsin@gmail.com>
2025-01-14 17:05:02 +0600

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "serif"],
+        coiny: ["Coiny", "serif"],
+        openSans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#fff9f0",
+        secondary: "#f7f2eb",
+        "dark-primary": "#081f5c",
+        "dark-secondary": "#334eac",
+        "btn-primary": "#bad6eb",
+        "btn-secondary": "#7096d1",
+        tertiary: "#d0e3ff",
+      },
+    },
+  },
+  plugins: [],
+};
