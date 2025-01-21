44964069afe5c4d88df021d52d63baf6efb887d2
Tahsin <alahitahsin@gmail.com>
2025-01-21 14:45:23 +0600

    created: dashboard SideBarMobile

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,9 @@
+import SideBarMobile from "../components/SideBarMobile";
+
 function DashboardPage() {
   return (
-    <main className="bg-red-500 dark:bg-dark-primary min-h-screen max-w-screen-xl mx-auto text-black dark:text-white font-openSans py-10">
-      DashboardPage
+    <main className="bg-primary dark:bg-dark-primary min-h-screen max-w-screen-xl mx-auto text-black dark:text-white font-openSans py-10 relative">
+      <SideBarMobile />
     </main>
   );
 }
